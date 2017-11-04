389f79af5f2f97dde038f020f0267907d03e0cb8
patajones <ricardo.bernardes@gmail.com>
2017-11-04 11:52:49 -0200

    validação de email já existente

--- src/main/java/br/com/patajones/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/patajones/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.patajones.cursomc.domain.Cliente;
 import br.com.patajones.cursomc.domain.enums.TipoCliente;
 import br.com.patajones.cursomc.dto.ClienteNewDTO;
+import br.com.patajones.cursomc.repositories.ClienteRepository;
 import br.com.patajones.cursomc.resources.exception.FieldMessage;
 import br.com.patajones.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (dto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(dto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CNPJ Inválido", "CpfOuCnpj"));
+		}
+		
+		Cliente c = repo.findByEmail(dto.getEmail());
+		if (c!=null) {
+			list.add(new FieldMessage("Email já existe.", "email"));
 		}
 
 		for (FieldMessage e : list) {
