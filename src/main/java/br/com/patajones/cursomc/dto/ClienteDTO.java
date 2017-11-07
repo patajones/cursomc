d312f57e28bbc14b84e3ca2683250533ad02e529
patajones <ricardo.bernardes@gmail.com>
2017-11-07 00:26:11 -0200

    email não repetido também na atualização (DTO normal)

--- src/main/java/br/com/patajones/cursomc/dto/ClienteDTO.java
+++ src/main/java/br/com/patajones/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import br.com.patajones.cursomc.domain.Cliente;
+import br.com.patajones.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
