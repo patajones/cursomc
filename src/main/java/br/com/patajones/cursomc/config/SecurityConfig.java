e4338fca457a0ab9962c27192f7f13fb5f4e3864
ricardob <ricardob@STJ171960.stj.gov.br>
2017-11-23 13:45:48 -0200

    Implementando autorização

--- src/main/java/br/com/patajones/cursomc/config/SecurityConfig.java
+++ src/main/java/br/com/patajones/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.patajones.cursomc.security.JWTAuthenticationFilter;
+import br.com.patajones.cursomc.security.JWTAuthorizationFilter;
 import br.com.patajones.cursomc.security.JWTUtil;
 
 @Configuration
@@ -48,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
